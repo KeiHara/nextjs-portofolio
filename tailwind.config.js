bdf97658adfeb9cb4f0f0ad9bc57264a8c0bc0f9
KeiHara <keihara453@gmail.com>
2022-02-05 23:36:54 +1300

    configuring prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   mode: 'jit',
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./comps/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './comps/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
