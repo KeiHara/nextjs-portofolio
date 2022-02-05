bc15bfc53918abcee7c069a667a0a264013faba1
KeiHara <keihara453@gmail.com>
2022-02-05 23:37:07 +1300

    adding content till bio

--- comps/SideBar.tsx
+++ comps/SideBar.tsx
@@ -1,31 +1,44 @@
-import { NextPage } from "next";
-import React, { createElement } from 'react'
-import { FaFire } from 'react-icons/fa'
-import { IconType } from "react-icons/lib";
-import { BiUserCircle } from 'react-icons/bi'
-import Link from "next/link";
-import styles from '../styles/SideBar.module.scss'
+import { NextPage } from 'next';
+import React, { createElement } from 'react';
+import { FaFire } from 'react-icons/fa';
+import { IconType } from 'react-icons/lib';
+import { BiUserCircle } from 'react-icons/bi';
+import Link from 'next/link';
+import styles from './SideBar.module.scss';
 
 interface Props {
-  icon: IconType
-  text?: string
+  icon: IconType;
+  text?: string;
 }
 
 const SideBar: NextPage = () => {
   return (
-    <div className="top-0 left-0 h-screen m-0 flex flex-col bg-gray-900 text-white shadow-lg w-20 sticky">
-      <Link href="/"><a><SideBarIcon icon={props => <BiUserCircle size={28}/>} text="about me 😊"/></a></Link>
-      <hr className={styles.hr}/>
-      <Link href="/about"><a><SideBarIcon icon={props => <FaFire size={28}/>}/></a></Link>
-      <SideBarIcon icon={props => <FaFire size={28}/>}/> 
-      <SideBarIcon icon={props => <FaFire size={28}/>}/> 
+    <div className="top-0 left-0 h-screen m-0 flex flex-col bg-zinc-800 text-white shadow-lg w-20 sticky">
+      <Link href="/">
+        <a>
+          <SideBarIcon
+            icon={(props) => <BiUserCircle size={28} />}
+            text="about me 😊"
+          />
+        </a>
+      </Link>
+      <hr className={styles.hr} />
+      <Link href="/about">
+        <a>
+          <SideBarIcon icon={(props) => <FaFire size={28} />} />
+        </a>
+      </Link>
+      <SideBarIcon icon={(props) => <FaFire size={28} />} />
+      <SideBarIcon icon={(props) => <FaFire size={28} />} />
     </div>
-  )
-}
+  );
+};
 
-const SideBarIcon= ({icon, text = 'tooltip 😀'}: Props) => (
+const SideBarIcon = ({ icon, text = 'tooltip 😀' }: Props) => (
   <div className={`${styles.icon} group`}>
-    <div className={`${styles.state} group-hover:scale-100 group-hover:h-5`}></div>
+    <div
+      className={`${styles.state} group-hover:scale-100 group-hover:h-5`}
+    ></div>
     {createElement(icon)}
     <span className={`${styles.tooltip} group-hover:scale-100`}>
       <div className={styles.triangle}>
@@ -34,6 +47,6 @@ const SideBarIcon= ({icon, text = 'tooltip 😀'}: Props) => (
       {text}
     </span>
   </div>
-)
+);
 
-export default SideBar
+export default SideBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,61 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import profile from '../public/profile_pic.jpg';
 
 const Home: NextPage = () => {
   return (
-    <div className="w-full h-full">
+    <div className="flex h-full w-full justify-center bg-zinc-900">
+      <div className="max-w-xl">
+        {/* Intro */}
+        <div className="flex pt-4">
+          <div>
+            <h1 className="text-4xl font-bold text-white">Keisuke Hara</h1>
+            <p className="text-white">
+              Digital Artist (Frontend Engineer / Musician)
+            </p>
+          </div>
+          {/* prof pic */}
+          <img
+            className="ml-10 w-24 rounded-full border-2"
+            src={profile.src}
+            alt="prof-pic"
+          />
+        </div>
+        {/* Work */}
+        <div className="mt-4 flex flex-col">
+          <h3 className="mb-2 w-fit text-xl font-bold text-white after:float-left after:h-1 after:w-full after:rounded-sm after:bg-zinc-500">
+            Work
+          </h3>
+          <p className="text-justify indent-4 text-white">
+            Keisuke is a second-year student studying a Bachelor of Science at
+            the University of Auckland, majoring in Computer Science. He is
+            passionate about taking on opportunities to actively improve my web
+            development skills and with his free time. He is currently working
+            as a Frontend engineer / Research assistant at the University
+            Auckland helping research project called&nbsp;
+            <a
+              className="inline-block origin-bottom-right indent-0 text-teal-300 transition-all duration-200 ease-linear after:float-left after:h-0.5 after:w-full after:origin-center after:scale-0 after:rounded-sm after:bg-teal-300 after:duration-200  hover:text-teal-200 hover:after:scale-100 hover:after:bg-teal-200"
+              href="https://habits-uat.xyz/"
+              target="_blank">
+              Habits
+            </a>
+            .
+          </p>
+        </div>
+        {/* Bio */}
+        <div className="mt-4 flex flex-col">
+          <h3 className="mb-2 w-fit text-xl font-bold text-white after:float-left after:h-1 after:w-full after:rounded-sm after:bg-zinc-500">
+            Bio
+          </h3>
+          <div>
+            <p className=" text-white">
+              <span className="mr-3 font-bold"> 2003</span>
+              Born in Tokyo(東京), Japan.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
