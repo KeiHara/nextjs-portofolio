f39def315ac9fa381113763d989bc79a0b984d70
KeiHara <keihara453@gmail.com>
2022-03-05 23:38:26 +1300

    making sideicon change color when route changes

--- comps/SideBar.tsx
+++ comps/SideBar.tsx
@@ -3,9 +3,11 @@ import { FaFire } from 'react-icons/fa';
 import { IconType } from 'react-icons/lib';
 import { BiUserCircle } from 'react-icons/bi';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 interface SideBarIconProps {
   icon: IconType;
+  toggled?: boolean;
   text?: string;
 }
 
@@ -37,6 +39,23 @@ const SideBar = () => {
       setIsDark(true);
     }
   };
+  const [toggledSideBarIcon, setToggledSideBarIcon] = useState([
+    true,
+    false,
+    false,
+    false,
+  ]);
+  const router = useRouter();
+  useEffect(() => {
+    switch (router.asPath) {
+      case '/':
+        setToggledSideBarIcon([true, false, false, false]);
+        break;
+      case '/works':
+        setToggledSideBarIcon([false, true, false, false]);
+        break;
+    }
+  }, [router.pathname]);
 
   return (
     <div className="fixed top-0 left-0 z-10 m-0 flex h-screen w-20 flex-col bg-neutral-200 py-2 shadow-lg dark:bg-zinc-800 dark:text-white">
@@ -45,13 +64,17 @@ const SideBar = () => {
           <SideBarIcon
             icon={(props) => <BiUserCircle size={28} />}
             text="about me 😊"
+            toggled={toggledSideBarIcon[0]}
           />
         </a>
       </Link>
       <hr className="mx-5 rounded-full border border-gray-100 bg-gray-100 dark:border-zinc-700 dark:bg-zinc-700" />
       <Link href="/works">
         <a>
-          <SideBarIcon icon={(props) => <FaFire size={28} />} />
+          <SideBarIcon
+            icon={(props) => <FaFire size={28} />}
+            toggled={toggledSideBarIcon[1]}
+          />
         </a>
       </Link>
       <SideBarIcon icon={(props) => <FaFire size={28} />} />
@@ -76,17 +99,29 @@ const SideBar = () => {
   );
 };
 
-const SideBarIcon = ({ icon, text = 'tooltip 😀' }: SideBarIconProps) => (
-  <div className="group relative mx-auto mt-2 mb-2 flex h-12 w-12 items-center justify-center rounded-3xl bg-neutral-100 shadow-md duration-200 ease-linear  hover:rounded-xl hover:bg-sky-300 dark:bg-zinc-700 hover:dark:bg-teal-300">
-    <div className="fixed left-[-4px] h-2 w-2 origin-left scale-0 rounded-full bg-sky-300 duration-300 group-hover:h-5 group-hover:scale-100 dark:bg-teal-300"></div>
-    {createElement(icon)}
-    <span className="absolute left-14 m-4 flex min-w-max origin-left scale-0 items-center rounded-md bg-neutral-200 p-2 text-sm font-bold shadow-md duration-100 group-hover:scale-100 dark:bg-zinc-800">
-      <div className="absolute left-[-7px] inline-block w-2 overflow-hidden">
-        <div className="h-3 origin-top-right -rotate-45 transform bg-neutral-200 dark:bg-zinc-800"></div>
-      </div>
-      {text}
-    </span>
-  </div>
-);
+const SideBarIcon = ({
+  icon,
+  toggled,
+  text = 'tooltip 😀',
+}: SideBarIconProps) => {
+  return (
+    <div
+      className={`${
+        toggled && 'rounded-xl bg-sky-300  dark:bg-teal-300'
+      } group relative mx-auto mt-2 mb-2 flex h-12 w-12 items-center justify-center rounded-3xl bg-neutral-100 shadow-md duration-200 ease-linear   hover:rounded-xl hover:bg-sky-300 dark:bg-zinc-700 hover:dark:bg-teal-300`}>
+      <div
+        className={`${
+          toggled && 'left-0 h-5 scale-100'
+        } fixed left-[-4px] h-2 w-2 origin-left scale-0 rounded-full bg-sky-300 duration-300 group-hover:h-5 group-hover:scale-100 dark:bg-teal-300`}></div>
+      {createElement(icon)}
+      <span className="absolute left-14 m-4 flex min-w-max origin-left scale-0 items-center rounded-md bg-neutral-200 p-2 text-sm font-bold shadow-md duration-100 group-hover:scale-100 dark:bg-zinc-800">
+        <div className="absolute left-[-7px] inline-block w-2 overflow-hidden">
+          <div className="h-3 origin-top-right -rotate-45 transform bg-neutral-200 dark:bg-zinc-800"></div>
+        </div>
+        {text}
+      </span>
+    </div>
+  );
+};
 
 export default SideBar;
