887150c37c337fbcde4366cdf26d4baaba90a21b
KeiHara <keihara453@gmail.com>
2022-03-05 22:49:40 +1300

    fixing topnav

--- comps/TopNav.tsx
+++ comps/TopNav.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 const TopNav = () => {
-  const [pageTitle, setPageTitle] = useState();
+  const [pageTitle, setPageTitle] = useState('');
   const router = useRouter();
   useEffect(() => {
     console.log(router.asPath);
@@ -21,7 +21,7 @@ const TopNav = () => {
     <div
       className="bg-neutral fixed flex h-16 w-full bg-neutral-200 shadow-lg dark:bg-zinc-800 dark:text-white
   ">
-      <h1 className="my-auto ml-5 ml-7 text-4xl font-bold text-black dark:text-white">
+      <h1 className="my-auto ml-2 text-4xl font-bold text-black dark:text-white">
         {pageTitle}
       </h1>
     </div>
