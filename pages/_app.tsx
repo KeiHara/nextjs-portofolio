218ade84b63852ab4359d3349429b8edf49f1130
keihara <keihara453@gmail.com>
2022-05-03 18:04:49 +1200

    added margin at the bottom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import Layout from '../comps/Layout';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <div className="flex w-full justify-center">
+      <div className="mb-4 flex w-full justify-center">
         <Component {...pageProps} />
       </div>
     </Layout>
