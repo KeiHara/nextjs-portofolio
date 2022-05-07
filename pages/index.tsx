dd7bf8ecfd4cbea05c551247aa088f21f9c4ea70
keihara <keihara453@gmail.com>
2022-05-07 13:01:54 +1200

    changed table item width

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,10 +47,10 @@ const Home: NextPage = () => {
           <p className="text-justify tracking-wide dark:text-white">
             Keisuke is a second-year student studying a Bachelor of Science at
             the University of Auckland, majoring in Computer Science. He is
-            passionate about taking on opportunities to actively improve my web
-            development skills. He is currently working as a Frontend engineer /
-            Research assistant at the University Auckland helping research
-            project called&nbsp;
+            passionate about taking on opportunities to improve his web
+            development skills actively. He is currently working as a Frontend
+            engineer / Research assistant at the University of Auckland, helping
+            research project called&nbsp;
             <a
               className="hover:text-sky500 inline-block origin-bottom-right indent-0 text-sky-700 duration-200 ease-linear after:float-left after:h-0.5 after:w-full after:origin-center after:scale-0 after:rounded-sm after:bg-sky-700 after:duration-200 hover:text-sky-600 hover:after:scale-100  hover:after:bg-sky-600 dark:text-teal-300 after:dark:bg-teal-300 hover:dark:text-teal-200 hover:after:dark:bg-teal-200"
               href="https://habits-uat.xyz/"
@@ -65,23 +65,27 @@ const Home: NextPage = () => {
           <h3 className="mb-2 w-fit text-xl font-bold after:float-left after:h-1 after:w-full after:rounded-sm after:bg-gray-700 dark:text-white after:dark:bg-zinc-500">
             Bio
           </h3>
-          <div className="grid grid-cols-2 gap-4">
-            <div className="font-bold dark:text-white">2003</div>
-            <p className="dark:text-white">Born in Tokyo(東京), Japan.</p>
+          <div className="grid grid-cols-[1fr_3fr] gap-4">
+            <div className="w-fit font-bold dark:text-white">2003</div>
+            <p className="dark:text-white">Born in Tokyo (東京), Japan.</p>
 
-            <div className="font-bold dark:text-white">2021</div>
+            <div className="w-fit font-bold dark:text-white">2021</div>
             <p className="dark:text-white">
               Started studying Computer Science at University of Auckland
             </p>
 
-            <div className="font-bold dark:text-white">2021 to present</div>
+            <div className="w-fit font-bold dark:text-white">
+              2021 to present
+            </div>
             <p className="dark:text-white">
               Working as a frontend(Angular) engineer at Kekeno Tech
             </p>
 
-            <div className="font-bold dark:text-white">2021 to present</div>
+            <div className="w-fit font-bold dark:text-white">
+              2021 to present
+            </div>
             <p className="dark:text-white">
-              Working as a front(Angular) engineer at university of auckland
+              Working as a Front (Angular) engineer at university of auckland
               helping a research project called&nbsp;
               <a
                 className="inline-block origin-bottom-right indent-0 text-sky-700 duration-200 ease-linear after:float-left after:h-0.5 after:w-full after:origin-center after:scale-0 after:rounded-sm after:bg-sky-700 after:duration-200 hover:text-sky-600 hover:after:scale-100  hover:after:bg-sky-600 dark:text-teal-300 after:dark:bg-teal-300 hover:dark:text-teal-200 hover:after:dark:bg-teal-200"
