0336f0bfef76f5e4c86bb9cac0e2ac765c99fd5f
keihara <keihara453@gmail.com>
2022-07-09 21:05:40 +1200

    changing about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,11 @@ const Home: NextPage = () => {
         {/* Intro */}
         <div className="flex justify-between pt-4">
           <div>
-            <h1 className="text-4xl font-bold text-black dark:text-white">
+            <h1 className="text-4xl font-bold  dark:text-white">
               Keisuke Hara
             </h1>
             <p className="dark:text-white">
-              Digital Artist (Frontend Engineer / Musician)
+              Software Engineer (Frontend Engineer / Musician)
             </p>
           </div>
           {/* prof pic */}
@@ -45,19 +45,18 @@ const Home: NextPage = () => {
             Work
           </h3>
           <p className="text-justify tracking-wide dark:text-white">
-            Keisuke is a second-year student studying a Bachelor of Science at
+            Keisuke is a penultimate student studying a Bachelor of Science at
             the University of Auckland, majoring in Computer Science. He is
             passionate about taking on opportunities to improve his web
             development skills actively. He is currently working as a Frontend
-            engineer / Research assistant at the University of Auckland, helping
-            research project called&nbsp;
+            engineer / Mobile app developer at{' '}
             <a
               className="hover:text-sky500 inline-block origin-bottom-right indent-0 text-sky-700 duration-200 ease-linear after:float-left after:h-0.5 after:w-full after:origin-center after:scale-0 after:rounded-sm after:bg-sky-700 after:duration-200 hover:text-sky-600 hover:after:scale-100  hover:after:bg-sky-600 dark:text-teal-300 after:dark:bg-teal-300 hover:dark:text-teal-200 hover:after:dark:bg-teal-200"
-              href="https://habits-uat.xyz/"
+              href="https://stage-dot-kekeno-site.ts.r.appspot.com/"
               target="_blank">
-              Habits
+              Kekeno Tech
             </a>
-            .
+            , developing variety of projects.
           </p>
         </div>
         {/* Bio */}
@@ -78,18 +77,22 @@ const Home: NextPage = () => {
               2021 to present
             </div>
             <p className="dark:text-white">
-              Working as a frontend(Angular) engineer at Kekeno Tech
+              Working as a frontend engineer at{' '}
+              <a
+                className="hover:text-sky500 inline-block origin-bottom-right indent-0 text-sky-700 duration-200 ease-linear after:float-left after:h-0.5 after:w-full after:origin-center after:scale-0 after:rounded-sm after:bg-sky-700 after:duration-200 hover:text-sky-600 hover:after:scale-100  hover:after:bg-sky-600 dark:text-teal-300 after:dark:bg-teal-300 hover:dark:text-teal-200 hover:after:dark:bg-teal-200"
+                href="https://stage-dot-kekeno-site.ts.r.appspot.com/"
+                target="_blank">
+                Kekeno Tech
+              </a>
             </p>
 
-            <div className="w-fit font-bold dark:text-white">
-              2021 to present
-            </div>
+            <div className="w-fit font-bold dark:text-white">2022 Summer</div>
             <p className="dark:text-white">
-              Working as a Front (Angular) engineer at university of auckland
-              helping a research project called&nbsp;
+              Working as a Front engineer at University of Auckland helping a
+              research project called&nbsp;
               <a
                 className="inline-block origin-bottom-right indent-0 text-sky-700 duration-200 ease-linear after:float-left after:h-0.5 after:w-full after:origin-center after:scale-0 after:rounded-sm after:bg-sky-700 after:duration-200 hover:text-sky-600 hover:after:scale-100  hover:after:bg-sky-600 dark:text-teal-300 after:dark:bg-teal-300 hover:dark:text-teal-200 hover:after:dark:bg-teal-200"
-                href="https://habits-uat.xyz/"
+                href="https://habits.school/"
                 target="_blank">
                 Habits
               </a>
