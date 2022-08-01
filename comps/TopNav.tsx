2795b11a5aa3c6782452a30da870b2665b72af13
Kei Hara <keihara453@gmail.com>
2022-08-02 02:57:48 +1200

    title alignment

--- comps/TopNav.tsx
+++ comps/TopNav.tsx
@@ -21,7 +21,7 @@ const TopNav = () => {
     <div
       className="fixed z-10 flex w-full justify-center bg-neutral-200/50 shadow-sm backdrop-blur-md dark:bg-zinc-800/50 dark:text-white
   ">
-      <div className="ml-3 w-full max-w-sm lg:max-w-2xl">
+      <div className="ml-3 w-full max-w-sm lg:-ml-1 lg:max-w-2xl phone:-ml-6">
         <h1 className="my-4 text-4xl font-bold text-black dark:text-white">
           {pageTitle}
         </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,11 @@ module.exports = {
   mode: 'jit',
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './comps/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
+  theme: {
+    extend: {
+      screens: {
+        phone: '432px',
+      },
+    },
+  },
 };
