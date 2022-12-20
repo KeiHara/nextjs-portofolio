7e94115113945efaa3372f61f05551151736fcf6
KeiHara <84754523+KeiHara@users.noreply.github.com>
2022-12-20 18:30:43 +0900

    adding framer motion

--- comps/SideBar.tsx
+++ comps/SideBar.tsx
@@ -1,20 +1,16 @@
-import React, { createElement, useEffect, useState } from 'react';
-import {
-  FaArrowLeft,
-  FaArrowRight,
-  FaGithub,
-  FaMoon,
-  FaSun,
-} from 'react-icons/fa';
-import { IconType } from 'react-icons/lib';
-import { BiUserCircle } from 'react-icons/bi';
-import { MdMusicNote, MdWorkOutline } from 'react-icons/md';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import React, { createElement, useEffect, useState } from "react";
+import { FaArrowLeft, FaArrowRight, FaGithub, FaMoon, FaSun } from "react-icons/fa";
+import { IconType } from "react-icons/lib";
+import { BiUserCircle } from "react-icons/bi";
+import { MdMusicNote, MdWorkOutline } from "react-icons/md";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { motion, Transition, Variants } from "framer-motion";
 
 interface SideBarIconProps {
   icon: IconType;
-  toggled?: boolean;
+  toggled: boolean;
+  isDark: boolean;
   text?: string;
 }
 
@@ -22,28 +18,29 @@ const SideBar = () => {
   const [isDark, setIsDark] = useState(false);
   useEffect(() => {
     if (
-      localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
-      document.documentElement.classList.add('dark');
+      document.documentElement.classList.add("dark");
       setIsDark(true);
-      localStorage.theme = 'dark';
+      localStorage.theme = "dark";
     } else {
-      document.documentElement.classList.remove('dark');
-      localStorage.theme = 'light';
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "light";
     }
-    return () => {};
+    return () => {
+    };
   }, []);
 
   const toggleTheme = (): void => {
     if (isDark) {
-      document.documentElement.classList.remove('dark');
-      localStorage.theme = 'light';
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "light";
       setIsDark(false);
     } else {
-      document.documentElement.classList.add('dark');
-      localStorage.theme = 'dark';
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "dark";
       setIsDark(true);
     }
   };
@@ -51,7 +48,7 @@ const SideBar = () => {
     true,
     false,
     false,
-    false,
+    false
   ]);
 
   const [onLeft, setOnRight] = useState(true);
@@ -59,10 +56,10 @@ const SideBar = () => {
   const router = useRouter();
   useEffect(() => {
     switch (router.asPath) {
-      case '/':
+      case "/":
         setToggledSideBarIcon([true, false, false, false]);
         break;
-      case '/works':
+      case "/works":
         setToggledSideBarIcon([false, true, false, false]);
         break;
     }
@@ -72,8 +69,8 @@ const SideBar = () => {
     <div
       className={`fixed z-50 m-0 flex h-full w-20 flex-col bg-neutral-200 py-2 shadow-lg transition-all duration-300 ease-in-out  dark:bg-zinc-800 dark:text-white  sm:top-2/4 sm:h-[500px] sm:max-h-[80%] sm:-translate-y-2/4  sm:rounded-2xl ${
         !onLeft
-          ? 'sm:left-[calc((100%-384px)/2+384px+40px)] lg:left-[calc((100%-672px)/2+672px+40px)]'
-          : 'sm:left-[calc((100%-384px)/2-130px)] lg:left-[calc((100%-672px)/2-130px)]'
+          ? "sm:left-[calc((100%-384px)/2+384px+40px)] lg:left-[calc((100%-672px)/2+672px+40px)]"
+          : "sm:left-[calc((100%-384px)/2-130px)] lg:left-[calc((100%-672px)/2-130px)]"
       }`}>
       <Link href="/">
         <a>
@@ -81,6 +78,7 @@ const SideBar = () => {
             icon={(props) => <BiUserCircle size={28} />}
             text="About me"
             toggled={toggledSideBarIcon[0]}
+            isDark={isDark}
           />
         </a>
       </Link>
@@ -91,6 +89,7 @@ const SideBar = () => {
             icon={(props) => <MdWorkOutline size={28} />}
             toggled={toggledSideBarIcon[1]}
             text="My works"
+            isDark={isDark}
           />
         </a>
       </Link>
@@ -100,6 +99,8 @@ const SideBar = () => {
             icon={(props) => <MdMusicNote size={28} />}
             toggled={toggledSideBarIcon[2]}
             text="My music"
+            isDark={isDark}
+
           />
         </a>
       </Link>
@@ -108,19 +109,22 @@ const SideBar = () => {
           icon={(props) => <FaGithub size={28} />}
           toggled={toggledSideBarIcon[3]}
           text="Source"
+          isDark={isDark}
         />
       </a>
 
       <div
         onClick={() => setOnRight(!onLeft)}
         className=" group relative mx-auto mt-auto mb-2 hidden h-12 w-12 cursor-pointer items-center justify-center rounded-3xl shadow-md duration-200 ease-linear hover:rounded-xl dark:bg-zinc-700  sm:flex">
-        <div className="absolute left-[-16px] h-2 w-1 origin-left scale-0 rounded-r-full  bg-black duration-300 group-hover:h-5 group-hover:scale-100 dark:bg-white"></div>
+        <div
+          className="absolute left-[-16px] h-2 w-1 origin-left scale-0 rounded-r-full  bg-black duration-300 group-hover:h-5 group-hover:scale-100 dark:bg-white"></div>
         {onLeft ? <FaArrowRight size={28} /> : <FaArrowLeft size={28} />}
-        <span className="absolute left-14 m-4 flex min-w-max origin-left scale-0 items-center rounded-md bg-neutral-200 p-2 text-sm font-bold shadow-md duration-100 group-hover:scale-100 dark:bg-zinc-800">
+        <span
+          className="absolute left-14 m-4 flex min-w-max origin-left scale-0 items-center rounded-md bg-neutral-200 p-2 text-sm font-bold shadow-md duration-100 group-hover:scale-100 dark:bg-zinc-800">
           <div className="absolute left-[-7px] inline-block w-2 overflow-hidden">
             <div className="h-3 origin-top-right -rotate-45 transform bg-neutral-200 dark:bg-zinc-800"></div>
           </div>
-          {onLeft ? 'Move to right' : 'Move to left'}
+          {onLeft ? "Move to right" : "Move to left"}
         </span>
       </div>
 
@@ -128,13 +132,15 @@ const SideBar = () => {
         onClick={() => toggleTheme()}
         className={`group relative mx-auto mb-2 mt-auto flex  h-12 w-12 cursor-pointer items-center justify-center rounded-3xl shadow-md duration-200 ease-linear hover:rounded-xl sm:mt-2 ${
           isDark
-            ? 'bg-purple-500 hover:bg-purple-600'
-            : 'bg-orange-400 hover:bg-orange-500'
+            ? "bg-purple-500 hover:bg-purple-600"
+            : "bg-orange-400 hover:bg-orange-500"
         } `}>
-        <div className="absolute left-[-16px] h-2 w-1 origin-left scale-0 rounded-r-full  bg-black duration-300 group-hover:h-5 group-hover:scale-100 dark:bg-white"></div>
+        <div
+          className="absolute left-[-16px] h-2 w-1 origin-left scale-0 rounded-r-full  bg-black duration-300 group-hover:h-5 group-hover:scale-100 dark:bg-white"></div>
         {isDark ? <FaMoon size={28} /> : <FaSun size={28} />}
 
-        <span className="absolute left-14 m-4 flex min-w-max origin-left scale-0 items-center rounded-md bg-neutral-200 p-2 text-sm font-bold shadow-md duration-100 group-hover:scale-100 dark:bg-zinc-800">
+        <span
+          className="absolute left-14 m-4 flex min-w-max origin-left scale-0 items-center rounded-md bg-neutral-200 p-2 text-sm font-bold shadow-md duration-100 group-hover:scale-100 dark:bg-zinc-800">
           <div className="absolute left-[-7px] inline-block w-2 overflow-hidden">
             <div className="h-3 origin-top-right -rotate-45 transform bg-neutral-200 dark:bg-zinc-800"></div>
           </div>
@@ -146,27 +152,69 @@ const SideBar = () => {
 };
 
 const SideBarIcon = ({
-  icon,
-  toggled,
-  text = 'tooltip 😀',
-}: SideBarIconProps) => {
+                       icon,
+                       toggled,
+                       isDark,
+                       text = "tooltip 😀"
+                     }: SideBarIconProps) => {
   return (
-    <div
-      className={`${
-        toggled && 'rounded-xl bg-sky-300 dark:bg-teal-300'
-      } group relative mx-auto mt-2 mb-2 flex h-12 w-12 items-center justify-center rounded-3xl shadow-md duration-200 ease-linear hover:rounded-xl hover:bg-sky-300 dark:bg-zinc-700 hover:dark:bg-teal-300`}>
-      <div
-        className={`${
-          toggled && 'left-[-16px] h-5 scale-100'
-        } absolute left-[-16px] h-2 w-1 origin-left scale-0 rounded-r-full bg-sky-300 duration-300 group-hover:h-5 group-hover:scale-100 dark:bg-teal-300`}></div>
-      {createElement(icon)}
-      <span className="absolute left-14 m-4 flex min-w-max origin-left scale-0 items-center rounded-md bg-neutral-200 p-2 text-sm font-bold shadow-md duration-100 group-hover:scale-100 dark:bg-zinc-800">
-        <div className="absolute left-[-7px] inline-block w-2 overflow-hidden">
+    <motion.div whileHover={"hover"} animate={toggled ? "toggled" : "unToggled"}
+                className="group relative py-2">
+      <motion.div
+        whileTap={{ scale: 0.9 }}
+        variants={{
+          toggled: {
+            borderRadius: 12
+          },
+          unToggled: {
+            borderRadius: 24
+          },
+          hover: {
+            scale: 1.1,
+            borderRadius: 12
+          }
+        }}
+        transition={{
+          scale: { type: "spring", bounce: 0.75 },
+          borderRadius: { type: "spring", bounce: 0.75 }
+        }}
+        className={`flex mx-auto h-12 w-12 items-center justify-center shadow-md ${toggled ? "bg-sky-300 dark:bg-teal-300" : "bg-neutral-200 dark:bg-zinc-700"} hover:bg-sky-300 dark:hover:bg-teal-300`}>
+        {createElement(icon)}
+      </motion.div>
+      <motion.div
+        variants={{
+          toggled: {
+            width: "4px"
+          },
+          unToggled: {
+            width: "0px"
+          },
+          hover: {
+            width: "4px"
+          }
+        }}
+        className={`absolute top-1/2 h-6 -translate-y-1/2 origin-left rounded-r-full bg-sky-300 dark:bg-teal-300`}></motion.div>
+      <motion.span
+        variants={{
+          hover: {
+            scale: 1
+          }
+        }}
+        style={{
+          originX: 0,
+          scale: 0,
+          position: "absolute",
+          top: "50%",
+          translateY: "-50%",
+          left: "120%"
+        }}
+        className="flex min-w-max items-center rounded-md bg-neutral-200 p-2 text-sm font-bold shadow-md dark:bg-zinc-800">
+        <div className="absolute left-0 -translate-x-full inline-block w-2 overflow-hidden">
           <div className="h-3 origin-top-right -rotate-45 transform bg-neutral-200 dark:bg-zinc-800"></div>
         </div>
         {text}
-      </span>
-    </div>
+      </ motion.span>
+    </motion.div>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
-import '../styles/globals.scss';
-import type { AppProps } from 'next/app';
-import Layout from '../comps/Layout';
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import Layout from "../comps/Layout";
+import { AnimatePresence } from "framer-motion";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <div className="mb-4 flex w-full justify-center">
-        <Component {...pageProps} />
-      </div>
-    </Layout>
+    <AnimatePresence initial={false}>
+      <Layout>
+        <div className="mb-4 flex w-full justify-center">
+          <Component {...pageProps} />
+        </div>
+      </Layout>
+    </AnimatePresence>
   );
 }
 
