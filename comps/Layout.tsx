3993fecdb182f0fd13b2da3288ea7ebc9fa72e21
KeiHara <84754523+KeiHara@users.noreply.github.com>
2022-12-23 00:07:36 +0900

    fixing bug

--- comps/Layout.tsx
+++ comps/Layout.tsx
@@ -36,7 +36,7 @@ const Layout = ({ children }: props) => {
           {!isMobile ? (
               <TopNav />
           ) : ( <div></div> )}
-          <div className="scrollbar overflow-auto sm:pt-[4.5rem] sm:h-screen flex-1">
+          <div className="scrollbar overflow-auto sm:pt-[4.5rem] sm:h-screen h-[calc(100vh-4.5rem)] mb-[4.5rem] sm:mb-0">
             <div className="mt-16 flex justify-center">
               <LazyVoxelDog />
             </div>
