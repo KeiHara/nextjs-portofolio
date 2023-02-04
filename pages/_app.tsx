a31741980f0a69e59e2e400a8cb4b49c906bbe14
KeiHara <84754523+KeiHara@users.noreply.github.com>
2023-02-04 18:25:53 +0900

    adding vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,19 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../comps/Layout";
 import { AnimatePresence } from "framer-motion";
-
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <Layout>
-        <AnimatePresence exitBeforeEnter initial={true}>
-        <div className="mb-4 flex w-full justify-center">
-          <Component {...pageProps} />
-        </div>
-        </AnimatePresence>
+        <>
+          <AnimatePresence exitBeforeEnter initial={true}>
+          <div className="mb-4 flex w-full justify-center">
+            <Component {...pageProps} />
+          </div>
+          </AnimatePresence>
+          <Analytics />
+        </>
       </Layout>
   );
 }
