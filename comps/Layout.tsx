37502c0d222000e1a43a1f1d1a3c9b9493e89793
KeiHara <84754523+KeiHara@users.noreply.github.com>
2023-02-17 12:29:00 +0900

    changing scroll bar style

--- comps/Layout.tsx
+++ comps/Layout.tsx
@@ -1,55 +1,46 @@
-import SideBar from './SideBar';
-import TopNav from './TopNav';
-import { useEffect, useState } from 'react';
-import dynamic from 'next/dynamic';
-import VoxelDogLoader from './voxel-dog-loader';
-import BottomNav from "./BottomNav";
-import { motion } from 'framer-motion';
+import SideBar from './SideBar'
+import TopNav from './TopNav'
+import { useEffect, useState } from 'react'
+import dynamic from 'next/dynamic'
+import VoxelDogLoader from './voxel-dog-loader'
+import BottomNav from './BottomNav'
 interface props {
-  children: JSX.Element;
+  children: JSX.Element
 }
 
 const LazyVoxelDog = dynamic(() => import('./voxel-dog'), {
   ssr: false,
   loading: () => <VoxelDogLoader />,
-});
+})
 
 const Layout = ({ children }: props) => {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    window.innerWidth < 640 ? setIsMobile(true) : setIsMobile(false);
+    window.innerWidth < 640 ? setIsMobile(true) : setIsMobile(false)
     window.addEventListener('resize', () => {
-      window.innerWidth < 640 ? setIsMobile(true) : setIsMobile(false);
-    });
-  });
+      window.innerWidth < 640 ? setIsMobile(true) : setIsMobile(false)
+    })
+  })
 
   return (
     <div>
-      {isMobile ? (
-        <div></div>
-      ) : (
-        <SideBar />
-        )}
+      {isMobile ? <div></div> : <SideBar />}
 
-      <div className="bg-neutral-300 dark:bg-zinc-900 w-full flex flex-col">
+      <div className="flex w-full flex-col bg-neutral-300 dark:bg-zinc-900">
         <div className="flex flex-col">
-          {!isMobile ? (
-              <TopNav />
-          ) : ( <div></div> )}
-          <div className="scrollbar overflow-auto sm:pt-[4.5rem] sm:h-screen h-[calc(100vh-4.5rem)] pb-12 sm:pb-0">
+          {!isMobile ? <TopNav /> : <div></div>}
+          <div className="scrollbar h-[calc(100vh-4.5rem)] overflow-auto pb-12 sm:h-screen sm:pt-[4.5rem] sm:pb-0">
             <div className="mt-16 flex justify-center">
               <LazyVoxelDog />
             </div>
             <div className="relative">{children}</div>
           </div>
-          {isMobile ? (
-              <BottomNav />
-          ): ( <div></div> )}
+          {isMobile ? <BottomNav /> : <div></div>}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
