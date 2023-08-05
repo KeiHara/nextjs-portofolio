d3758c18e358b1d1b768642751b6b27a6c3c9039
KeiHara <84754523+KeiHara@users.noreply.github.com>
2023-08-06 03:31:30 +1200

    adding tooltip

--- comps/SideBar.tsx
+++ comps/SideBar.tsx
@@ -1,82 +1,83 @@
-import React, { createElement, useEffect, useState } from 'react'
+import React, { createElement, useEffect, useState } from "react";
 import {
   FaArrowLeft,
   FaArrowRight,
   FaGithub,
   FaMoon,
-  FaSun,
-} from 'react-icons/fa'
-import { IconType } from 'react-icons/lib'
-import { BiUserCircle } from 'react-icons/bi'
-import { MdWorkOutline } from 'react-icons/md'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { motion } from 'framer-motion'
+  FaSun
+} from "react-icons/fa";
+import { IconType } from "react-icons/lib";
+import { BiUserCircle } from "react-icons/bi";
+import { MdWorkOutline } from "react-icons/md";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 
 interface SideBarIcon {
-  icon: IconType
-  toggled: boolean
-  isDark: boolean
-  text?: string
+  icon: IconType;
+  toggled: boolean;
+  isDark: boolean;
+  text?: string;
 }
 
 const SideBar = () => {
-  const [isDark, setIsDark] = useState(false)
+  const [isDark, setIsDark] = useState(false);
 
   const toggleTheme = (): void => {
     if (isDark) {
-      document.documentElement.classList.remove('dark')
-      localStorage.theme = 'light'
-      setIsDark(false)
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "light";
+      setIsDark(false);
     } else {
-      document.documentElement.classList.add('dark')
-      localStorage.theme = 'dark'
-      setIsDark(true)
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "dark";
+      setIsDark(true);
     }
-  }
+  };
   const [toggledSideBarIcon, setToggledSideBarIcon] = useState([
     true,
     false,
     false,
-    false,
-  ])
+    false
+  ]);
 
-  const [isOnLeft, setIsOnLeft] = useState(true)
+  const [isOnLeft, setIsOnLeft] = useState(true);
 
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
     switch (router.asPath) {
-      case '/':
-        setToggledSideBarIcon([true, false, false, false])
-        break
-      case '/works':
-        setToggledSideBarIcon([false, true, false, false])
-        break
+      case "/":
+        setToggledSideBarIcon([true, false, false, false]);
+        break;
+      case "/works":
+        setToggledSideBarIcon([false, true, false, false]);
+        break;
     }
-  }, [router.asPath])
+  }, [router.asPath]);
 
   useEffect(() => {
     if (
-      localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
-      document.documentElement.classList.add('dark')
-      setIsDark(true)
-      localStorage.theme = 'dark'
+      document.documentElement.classList.add("dark");
+      setIsDark(true);
+      localStorage.theme = "dark";
     } else {
-      document.documentElement.classList.remove('dark')
-      localStorage.theme = 'light'
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "light";
     }
-    return () => {}
-  }, [isDark])
+    return () => {
+    };
+  }, [isDark]);
 
   return (
     <div
       className={`fixed z-50 m-0 flex h-full w-20 flex-col bg-neutral-200 py-2 shadow-lg transition-all duration-300 ease-in-out  dark:bg-zinc-800 dark:text-white  sm:top-2/4 sm:h-[500px] sm:max-h-[80%] sm:-translate-y-2/4  sm:rounded-2xl ${
         !isOnLeft
-          ? 'sm:left-[calc((100%-384px)/2+384px+40px)] lg:left-[calc((100%-672px)/2+672px+40px)]'
-          : 'sm:left-[calc((100%-384px)/2-130px)] lg:left-[calc((100%-672px)/2-130px)]'
+          ? "sm:left-[calc((100%-384px)/2+384px+40px)] lg:left-[calc((100%-672px)/2+672px+40px)]"
+          : "sm:left-[calc((100%-384px)/2-130px)] lg:left-[calc((100%-672px)/2-130px)]"
       }`}
     >
       <Link href="/">
@@ -113,8 +114,8 @@ const SideBar = () => {
         />
       </a>
       <motion.div
-        whileHover={['hover', isDark ? 'darkHighlight' : 'lightHighlight']}
-        animate={['initial', isDark ? 'dark' : 'light']}
+        whileHover={["hover", isDark ? "darkHighlight" : "lightHighlight"]}
+        animate={["initial", isDark ? "dark" : "light"]}
         className="group relative mt-auto cursor-pointer py-2"
         onClick={() => setIsOnLeft(!isOnLeft)}
       >
@@ -124,22 +125,22 @@ const SideBar = () => {
           variants={{
             initial: {
               scale: 1,
-              borderRadius: 24,
+              borderRadius: 24
             },
             hover: {
               scale: 1.1,
-              borderRadius: 12,
+              borderRadius: 12
             },
             dark: {
-              backgroundColor: 'rgb(63 63 70)',
+              backgroundColor: "rgb(63 63 70)"
             },
             light: {
-              backgroundColor: 'rgb(245 245 245)',
-            },
+              backgroundColor: "rgb(245 245 245)"
+            }
           }}
           transition={{
-            scale: { type: 'spring', bounce: 0.75 },
-            borderRadius: { type: 'spring', bounce: 0.75 },
+            scale: { type: "spring", bounce: 0.75 },
+            borderRadius: { type: "spring", bounce: 0.75 }
           }}
           className="mx-auto flex h-12 w-12 items-center justify-center shadow-md"
         >
@@ -148,49 +149,49 @@ const SideBar = () => {
         <motion.div
           variants={{
             hover: {
-              height: '1.25rem',
+              height: "1.25rem"
             },
             darkHighlight: {
-              backgroundColor: 'rgb(255 255 255)',
+              backgroundColor: "rgb(255 255 255)"
             },
             lightHighlight: {
-              backgroundColor: 'rgb(0 0 0)',
-            },
+              backgroundColor: "rgb(0 0 0)"
+            }
           }}
           transition={{
             duration: 0.2,
-            ease: 'easeInOut',
+            ease: "easeInOut"
           }}
           className={`absolute top-1/2 w-1 origin-left -translate-y-1/2 rounded-r-full`}
         ></motion.div>
         <motion.span
           variants={{
             hover: {
-              scale: 1,
-            },
+              scale: 1
+            }
           }}
           style={{
             originX: 0,
             scale: 0,
-            position: 'absolute',
-            top: '50%',
-            translateY: '-50%',
-            left: '120%',
+            position: "absolute",
+            top: "50%",
+            translateY: "-50%",
+            left: "120%"
           }}
           className="flex min-w-max items-center rounded-md bg-neutral-100 p-2 text-sm font-bold shadow-md dark:bg-zinc-800"
         >
           <div className="absolute left-0 inline-block w-2 -translate-x-full overflow-hidden">
             <div className="h-3 origin-top-right -rotate-45 transform bg-neutral-100 dark:bg-zinc-800"></div>
           </div>
-          {isOnLeft ? 'Move to right' : 'Move to left'}
+          {isOnLeft ? "Move to right" : "Move to left"}
         </motion.span>
       </motion.div>
 
       {/* theme button */}
       <motion.div
-        whileHover={['hover', isDark ? 'darkHighlight' : 'lightHighlight']}
-        animate={['initial', isDark ? 'dark' : 'light']}
-        whileTap={'tap'}
+        whileHover={["hover", isDark ? "darkHighlight" : "lightHighlight"]}
+        animate={["initial", isDark ? "dark" : "light"]}
+        whileTap={"tap"}
         className="group relative cursor-pointer py-2"
         onClick={() => toggleTheme()}
       >
@@ -199,31 +200,31 @@ const SideBar = () => {
           variants={{
             initial: {
               scale: 1,
-              borderRadius: 24,
+              borderRadius: 24
             },
             tap: {
-              scale: 0.9,
+              scale: 0.9
             },
             hover: {
               scale: 1.1,
-              borderRadius: 12,
+              borderRadius: 12
             },
             dark: {
-              backgroundColor: 'rgb(168 85 247)',
+              backgroundColor: "rgb(168 85 247)"
             },
             light: {
-              backgroundColor: 'rgb(251 146 60)',
+              backgroundColor: "rgb(251 146 60)"
             },
             darkHighlight: {
-              backgroundColor: 'rgb(147 51 234)',
+              backgroundColor: "rgb(147 51 234)"
             },
             lightHighlight: {
-              backgroundColor: 'rgb(249 115 22)',
-            },
+              backgroundColor: "rgb(249 115 22)"
+            }
           }}
           transition={{
-            scale: { type: 'spring', bounce: 0.75 },
-            borderRadius: { type: 'spring', bounce: 0.75 },
+            scale: { type: "spring", bounce: 0.75 },
+            borderRadius: { type: "spring", bounce: 0.75 }
           }}
           className="mx-auto flex h-12 w-12 justify-center overflow-hidden shadow-md"
         >
@@ -232,12 +233,12 @@ const SideBar = () => {
             variants={{
               dark: {
                 rotateZ: 180,
-                transition: { duration: 0.75, type: 'spring' },
+                transition: { duration: 0.75, type: "spring" }
               },
               light: {
                 rotateZ: 0,
-                transition: { duration: 0.75, type: 'spring' },
-              },
+                transition: { duration: 0.75, type: "spring" }
+              }
             }}
           >
             <FaSun size={28} />
@@ -247,34 +248,34 @@ const SideBar = () => {
         <motion.div
           variants={{
             hover: {
-              height: '1.25rem',
+              height: "1.25rem"
             },
             darkHighlight: {
-              backgroundColor: 'rgb(255 255 255)',
+              backgroundColor: "rgb(255 255 255)"
             },
             lightHighlight: {
-              backgroundColor: 'rgb(0 0 0)',
-            },
+              backgroundColor: "rgb(0 0 0)"
+            }
           }}
           transition={{
             duration: 0.2,
-            ease: 'easeInOut',
+            ease: "easeInOut"
           }}
           className={`absolute top-1/2 w-1 origin-left -translate-y-1/2 rounded-r-full`}
         ></motion.div>
         <motion.span
           variants={{
             hover: {
-              scale: 1,
-            },
+              scale: 1
+            }
           }}
           style={{
             originX: 0,
             scale: 0,
-            position: 'absolute',
-            top: '50%',
-            translateY: '-50%',
-            left: '120%',
+            position: "absolute",
+            top: "50%",
+            translateY: "-50%",
+            left: "120%"
           }}
           className="flex min-w-max items-center rounded-md bg-neutral-100 p-2 text-sm font-bold shadow-md dark:bg-zinc-800"
         >
@@ -285,25 +286,25 @@ const SideBar = () => {
         </motion.span>
       </motion.div>
     </div>
-  )
-}
+  );
+};
 
 const SideBarIcon = ({
-  icon,
-  toggled,
-  isDark,
-  text = 'tooltip 😀',
-}: SideBarIcon) => {
+                       icon,
+                       toggled,
+                       isDark,
+                       text = "tooltip 😀"
+                     }: SideBarIcon) => {
   return (
     <motion.div
       whileHover={[
-        !toggled ? 'hover' : '',
-        isDark ? 'darkHighlight' : 'lightHighlight',
+        !toggled ? "hover" : "",
+        isDark ? "darkHighlight" : "lightHighlight"
       ]}
       animate={
         toggled
-          ? ['toggled', isDark ? 'darkHighlight' : 'lightHighlight']
-          : ['unToggled', isDark ? 'dark' : 'light']
+          ? ["toggled", isDark ? "darkHighlight" : "lightHighlight"]
+          : ["unToggled", isDark ? "dark" : "light"]
       }
       className="group relative py-2"
     >
@@ -312,31 +313,31 @@ const SideBarIcon = ({
         initial={false}
         variants={{
           toggled: {
-            borderRadius: 12,
+            borderRadius: 12
           },
           unToggled: {
-            borderRadius: 24,
+            borderRadius: 24
           },
           hover: {
             scale: 1.1,
-            borderRadius: 12,
+            borderRadius: 12
           },
           dark: {
-            backgroundColor: 'rgb(63 63 70)',
+            backgroundColor: "rgb(63 63 70)"
           },
           light: {
-            backgroundColor: 'rgb(245 245 245)',
+            backgroundColor: "rgb(245 245 245)"
           },
           darkHighlight: {
-            backgroundColor: 'rgb(94 234 212)',
+            backgroundColor: "rgb(94 234 212)"
           },
           lightHighlight: {
-            backgroundColor: 'rgb(125 211 252)',
-          },
+            backgroundColor: "rgb(125 211 252)"
+          }
         }}
         transition={{
-          scale: { type: 'spring', bounce: 0.75 },
-          borderRadius: { type: 'spring', bounce: 0.75 },
+          scale: { type: "spring", bounce: 0.75 },
+          borderRadius: { type: "spring", bounce: 0.75 }
         }}
         className={`mx-auto flex h-12 w-12 items-center justify-center shadow-md`}
       >
@@ -345,34 +346,34 @@ const SideBarIcon = ({
       <motion.div
         variants={{
           toggled: {
-            height: '2rem',
+            height: "2rem"
           },
           unToggled: {
-            height: '0rem',
+            height: "0rem"
           },
           hover: {
-            height: '1.25rem',
-          },
+            height: "1.25rem"
+          }
         }}
         transition={{
           duration: 0.2,
-          ease: 'easeInOut',
+          ease: "easeInOut"
         }}
         className={`absolute top-1/2 w-1 origin-left -translate-y-1/2 rounded-r-full bg-sky-300 dark:bg-teal-300`}
       ></motion.div>
       <motion.span
         variants={{
           hover: {
-            scale: 1,
-          },
+            scale: 1
+          }
         }}
         style={{
           originX: 0,
           scale: 0,
-          position: 'absolute',
-          top: '50%',
-          translateY: '-50%',
-          left: '120%',
+          position: "absolute",
+          top: "50%",
+          translateY: "-50%",
+          left: "120%"
         }}
         className="flex min-w-max items-center rounded-md bg-neutral-100 p-2 text-sm font-bold shadow-md dark:bg-zinc-800"
       >
@@ -382,7 +383,7 @@ const SideBarIcon = ({
         {text}
       </motion.span>
     </motion.div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,55 +1,58 @@
-import Head from 'next/head'
-import worksJson from './works.json'
-import { MotionValue, motion, useScroll, useTransform } from 'framer-motion'
-import { useEffect, useRef, useState } from 'react'
+import Head from "next/head";
+import worksJson from "./works.json";
+import { MotionValue, motion, useScroll, useTransform } from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
 
 const Works = () => {
-  const works = worksJson
-  const titleRef = useRef<HTMLButtonElement>(null)
-  const worksContainerRef = useRef<HTMLDivElement>(null)
-  const webappContainerRef = useRef<HTMLDivElement>(null)
-  const appContainerRef = useRef<HTMLDivElement>(null)
-  const [isApp, setIsApp] = useState(false)
+  const works = worksJson;
+  const titleRef = useRef<HTMLButtonElement>(null);
+  const worksContainerRef = useRef<HTMLDivElement>(null);
+  const webappContainerRef = useRef<HTMLDivElement>(null);
+  const appContainerRef = useRef<HTMLDivElement>(null);
+  const [isApp, setIsApp] = useState(false);
   const { scrollXProgress } = useScroll({
-    container: worksContainerRef,
-  })
+    container: worksContainerRef
+  });
+
+  const [clickMeClicked, setClickMeClicked] = useState(false);
 
   const useParallax = (value: MotionValue<number>, distance: number) => {
-    return useTransform(value, [0, 1], [0, -distance])
-  }
+    return useTransform(value, [0, 1], [0, -distance]);
+  };
 
-  const y = useParallax(scrollXProgress, 28)
+  const y = useParallax(scrollXProgress, 28);
 
   useEffect(() => {
     scrollXProgress.onChange((value) => {
       if (value === 0) {
-        setIsApp(false)
+        setIsApp(false);
       } else if (value === 1) {
-        setIsApp(true)
+        setIsApp(true);
       }
-    })
-  })
+    });
+  });
 
   const toggleWorkType = () => {
     if (isApp) {
       worksContainerRef.current?.scroll({
         left: 0,
-        behavior: 'smooth',
-      })
+        behavior: "smooth"
+      });
     } else {
       worksContainerRef.current?.scroll({
         left: worksContainerRef.current.offsetWidth,
-        behavior: 'smooth',
-      })
+        behavior: "smooth"
+      });
     }
-  }
+    setClickMeClicked(true);
+  };
 
   return (
     <motion.div
       className="mx-3 max-w-sm overflow-x-hidden lg:max-w-2xl"
       variants={{
         hidden: { opacity: 0, x: 0, y: 200 },
-        enter: { opacity: 1, x: 0, y: 0 },
+        enter: { opacity: 1, x: 0, y: 0 }
       }}
       initial="hidden"
       animate="enter"
@@ -58,31 +61,57 @@ const Works = () => {
         <title>My works</title>
       </Head>
       <div className="flex w-full flex-col items-center">
-        <div className="mb-2 flex w-full">
-          <h3 className="w-fit text-xl font-bold text-black after:float-left after:h-1 after:w-full after:rounded-sm after:bg-gray-700 dark:text-white after:dark:bg-zinc-500">
+        <div className="mb-2 mt-3 flex w-full">
+          <h3
+            className="w-fit text-xl font-bold text-black after:float-left after:h-1 after:w-full after:rounded-sm after:bg-gray-700 dark:text-white after:dark:bg-zinc-500">
             Work
           </h3>
           <h3 className="mx-2 w-fit text-xl font-bold text-black dark:text-white">
             -
           </h3>
-          <button
-            onClick={() => toggleWorkType()}
-            ref={titleRef}
-            className="h-7 cursor-pointer overflow-hidden"
+          <motion.div className="relative group"
+                      whileHover="hover"
           >
-            <motion.h3
-              style={{ y }}
-              className="w-fit text-xl font-bold text-black dark:text-white"
+            <motion.span
+              variants={{
+                hover: {
+                  scale: 0
+                }
+              }}
+              style={{
+                originX: 0,
+                scale: 1,
+                position: "absolute",
+                top: "50%",
+                translateY: "-50%",
+                left: isApp ? "120%" : "90%"
+              }}
+              className={`flex text-white min-w-max items-center rounded-md p-2 text-sm font-bold shadow-md bg-sky-300  dark:bg-teal-300 ${clickMeClicked && "hidden"}`}
             >
-              Website
-            </motion.h3>
-            <motion.h3
-              style={{ y }}
-              className="w-fit text-xl font-bold text-black dark:text-white"
+              <div className="absolute left-0 inline-block w-2 -translate-x-full overflow-hidden">
+                <div className="h-3 origin-top-right -rotate-45 transform bg-sky-300  dark:bg-teal-300"></div>
+              </div>
+              Click me!
+            </motion.span>
+            <button
+              onClick={() => toggleWorkType()}
+              ref={titleRef}
+              className="h-7 cursor-pointer overflow-hidden"
             >
-              Mobile App
-            </motion.h3>
-          </button>
+              <motion.h3
+                style={{ y }}
+                className="w-fit text-xl font-bold text-black dark:text-white"
+              >
+                Website
+              </motion.h3>
+              <motion.h3
+                style={{ y }}
+                className="w-fit text-xl font-bold text-black dark:text-white"
+              >
+                Mobile App
+              </motion.h3>
+            </button>
+          </motion.div>
         </div>
         <motion.div
           ref={worksContainerRef}
@@ -93,7 +122,7 @@ const Works = () => {
             className="grid h-fit min-w-full snap-center gap-8 lg:grid-cols-2 lg:gap-4"
           >
             {works
-              .filter((work) => work.type === 'website')
+              .filter((work) => work.type === "website")
               .map((work, i) => (
                 <WorkCard
                   key={i}
@@ -110,7 +139,7 @@ const Works = () => {
             className="grid h-fit min-w-full snap-center gap-8 lg:grid-cols-2 lg:gap-4"
           >
             {works
-              .filter((work) => work.type === 'app')
+              .filter((work) => work.type === "app")
               .map((work, i) => (
                 <WorkCard
                   key={i}
@@ -125,43 +154,43 @@ const Works = () => {
         </motion.div>
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
 interface workCardProps {
-  href: string
-  imgSrcs?: string[]
-  imgSrc: string
-  title: string
-  description: string
+  href: string;
+  imgSrcs?: string[];
+  imgSrc: string;
+  title: string;
+  description: string;
 }
 
 const WorkCard = ({
-  imgSrc,
-  title,
-  description,
-  imgSrcs,
-  href,
-}: workCardProps) => {
-  const [isLoaded, setIsLoaded] = useState(false)
+                    imgSrc,
+                    title,
+                    description,
+                    imgSrcs,
+                    href
+                  }: workCardProps) => {
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
     <motion.a
       draggable={false}
-      whileHover={'hover'}
+      whileHover={"hover"}
       className="group rounded-md p-4 hover:bg-neutral-200 dark:hover:bg-zinc-800"
       target="_blank"
       variants={{
         hover: {
           boxShadow:
-            '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
-        },
+            "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)"
+        }
       }}
       href={href}
     >
       <div>
         <div
           className={`flex h-36 ${
-            imgSrcs ? 'justify-between' : 'justify-center'
+            imgSrcs ? "justify-between" : "justify-center"
           }`}
         >
           {imgSrcs ? (
@@ -171,10 +200,10 @@ const WorkCard = ({
                 key={i}
                 variants={{
                   hover: {
-                    scale: 1.05,
-                  },
+                    scale: 1.05
+                  }
                 }}
-                className={`rounded-md ${!isLoaded ?? 'blur-sm'}}`}
+                className={`rounded-md ${!isLoaded ?? "blur-sm"}}`}
                 src={src}
                 alt="img"
                 draggable={false}
@@ -184,8 +213,8 @@ const WorkCard = ({
             <motion.img
               variants={{
                 hover: {
-                  scale: 1.05,
-                },
+                  scale: 1.05
+                }
               }}
               className="rounded-md"
               src={imgSrc}
@@ -210,7 +239,7 @@ const WorkCard = ({
         </motion.p>
       </div>
     </motion.a>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
