0ea73b6fecdbe4b8319379ab0cd10b4448079bb2
KeiHara <keihara453@gmail.com>
2023-08-22 20:08:29 +1200

    adding icons to work

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,58 +1,59 @@
-import Head from 'next/head'
-import worksJson from './works.json'
-import { MotionValue, motion, useScroll, useTransform } from 'framer-motion'
-import React, { useEffect, useRef, useState } from 'react'
+import Head from "next/head";
+import worksJson from "./works.json";
+import { motion, MotionValue, useScroll, useTransform } from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
 
 const Works = () => {
-  const works = worksJson
-  const titleRef = useRef<HTMLButtonElement>(null)
-  const worksContainerRef = useRef<HTMLDivElement>(null)
-  const webappContainerRef = useRef<HTMLDivElement>(null)
-  const appContainerRef = useRef<HTMLDivElement>(null)
-  const [isApp, setIsApp] = useState(false)
+  const works = worksJson;
+  const titleRef = useRef<HTMLButtonElement>(null);
+  const worksContainerRef = useRef<HTMLDivElement>(null);
+  const webappContainerRef = useRef<HTMLDivElement>(null);
+  const appContainerRef = useRef<HTMLDivElement>(null);
+  const [isApp, setIsApp] = useState(false);
   const { scrollXProgress } = useScroll({
-    container: worksContainerRef,
-  })
+    container: worksContainerRef
+  });
 
-  const [clickMeClicked, setClickMeClicked] = useState(false)
+  const [clickMeClicked, setClickMeClicked] = useState(false);
 
   const useParallax = (value: MotionValue<number>, distance: number) => {
-    return useTransform(value, [0, 1], [0, -distance])
-  }
+    return useTransform(value, [0, 1], [0, -distance]);
+  };
 
-  const y = useParallax(scrollXProgress, 28)
+  const y = useParallax(scrollXProgress, 28);
 
   useEffect(() => {
     scrollXProgress.onChange((value) => {
       if (value === 0) {
-        setIsApp(false)
+        setIsApp(false);
       } else if (value === 1) {
-        setIsApp(true)
+        setIsApp(true);
       }
-    })
-  })
+    });
+  });
 
   const toggleWorkType = () => {
     if (isApp) {
       worksContainerRef.current?.scroll({
         left: 0,
-        behavior: 'smooth',
-      })
+        behavior: "smooth"
+      });
     } else {
       worksContainerRef.current?.scroll({
         left: worksContainerRef.current.offsetWidth,
-        behavior: 'smooth',
-      })
+        behavior: "smooth"
+      });
     }
-    setClickMeClicked(true)
-  }
+    setClickMeClicked(true);
+  };
 
   return (
     <motion.div
       className="mx-3 max-w-sm overflow-x-hidden lg:max-w-2xl"
       variants={{
         hidden: { opacity: 0, x: 0, y: 200 },
-        enter: { opacity: 1, x: 0, y: 0 },
+        enter: { opacity: 1, x: 0, y: 0 }
       }}
       initial="hidden"
       animate="enter"
@@ -62,7 +63,8 @@ const Works = () => {
       </Head>
       <div className="flex w-full flex-col items-center">
         <div className="mb-2 mt-3 flex w-full">
-          <h3 className="w-fit text-xl font-bold text-black after:float-left after:h-1 after:w-full after:rounded-sm after:bg-gray-700 dark:text-white after:dark:bg-zinc-500">
+          <h3
+            className="w-fit text-xl font-bold text-black after:float-left after:h-1 after:w-full after:rounded-sm after:bg-gray-700 dark:text-white after:dark:bg-zinc-500">
             Work
           </h3>
           <h3 className="mx-2 w-fit text-xl font-bold text-black dark:text-white">
@@ -72,19 +74,19 @@ const Works = () => {
             <motion.span
               variants={{
                 hover: {
-                  scale: 0,
-                },
+                  scale: 0
+                }
               }}
               style={{
                 originX: 0,
                 scale: 1,
-                position: 'absolute',
-                top: '50%',
-                translateY: '-50%',
-                left: isApp ? '120%' : '90%',
+                position: "absolute",
+                top: "50%",
+                translateY: "-50%",
+                left: isApp ? "120%" : "90%"
               }}
               className={`flex min-w-max items-center rounded-md bg-sky-300 p-2 text-sm font-bold text-white shadow-md  dark:bg-teal-300 ${
-                clickMeClicked && 'hidden'
+                clickMeClicked && "hidden"
               }`}
             >
               <div className="absolute left-0 inline-block w-2 -translate-x-full overflow-hidden">
@@ -114,14 +116,14 @@ const Works = () => {
         </div>
         <motion.div
           ref={worksContainerRef}
-          className="scrollbar flex w-full snap-x snap-mandatory overflow-x-auto"
+          className="scrollbar flex w-full snap-x snap-mandatory overflow-x-auto py-2"
         >
           <motion.div
             ref={webappContainerRef}
             className="grid h-fit min-w-full snap-center gap-8 lg:grid-cols-2 lg:gap-4"
           >
             {works
-              .filter((work) => work.type === 'website')
+              .filter((work) => work.type === "website")
               .map((work, i) => (
                 <WorkCard
                   key={i}
@@ -130,6 +132,7 @@ const Works = () => {
                   imgSrc={work.imgSrc}
                   title={work.title}
                   description={work.description}
+                  icons={work.icons}
                 />
               ))}
           </motion.div>
@@ -138,7 +141,7 @@ const Works = () => {
             className="grid h-fit min-w-full snap-center gap-8 lg:grid-cols-2 lg:gap-4"
           >
             {works
-              .filter((work) => work.type === 'app')
+              .filter((work) => work.type === "app")
               .map((work, i) => (
                 <WorkCard
                   key={i}
@@ -147,81 +150,106 @@ const Works = () => {
                   imgSrc={work.imgSrc}
                   title={work.title}
                   description={work.description}
+                  icons={work.icons}
                 />
               ))}
           </motion.div>
         </motion.div>
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
 interface workCardProps {
-  href: string
-  imgSrcs?: string[]
-  imgSrc: string
-  title: string
-  description: string
+  href: string;
+  imgSrcs?: string[];
+  imgSrc: string;
+  title: string;
+  description: string;
+  icons?: string[];
 }
 
 const WorkCard = ({
-  imgSrc,
-  title,
-  description,
-  imgSrcs,
-  href,
-}: workCardProps) => {
-  const [isLoaded, setIsLoaded] = useState(false)
+                    imgSrc,
+                    title,
+                    description,
+                    imgSrcs,
+                    href,
+                    icons
+                  }: workCardProps) => {
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
     <motion.a
       draggable={false}
-      whileHover={'hover'}
+      whileHover={"hover"}
       className="group rounded-md p-4 hover:bg-neutral-200 dark:hover:bg-zinc-800"
       target="_blank"
       variants={{
         hover: {
           boxShadow:
-            '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
-        },
+            "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)"
+        }
       }}
       href={href}
     >
-      <div>
-        <div
-          className={`flex h-36 ${
-            imgSrcs ? 'justify-between' : 'justify-center'
+      <div className="flex justify-center">
+        <motion.div
+          className={`flex h-36 rounded-md overflow-hidden relative ${
+            imgSrcs ? "justify-between w-full" : "justify-center w-fit group-hover:border-2"
           }`}
+          variants={{
+            hover: {
+              scale: 1.05
+            }
+          }}
         >
           {imgSrcs ? (
             imgSrcs.map((src, i) => (
-              <motion.img
-                onLoad={() => setIsLoaded(true)}
-                key={i}
-                variants={{
-                  hover: {
-                    scale: 1.05,
-                  },
-                }}
-                className={`rounded-md ${!isLoaded ?? 'blur-sm'}}`}
-                src={src}
-                alt="img"
-                draggable={false}
-              />
+              <div className="flex overflow-hidden rounded-md group-hover:border-2 relative">
+                <motion.img
+                  onLoad={() => setIsLoaded(true)}
+                  key={i}
+                  className={`rounded-md group-hover:blur-sm ${!isLoaded ?? "blur-sm"}}`}
+                  src={src}
+                  alt="img"
+                  draggable={false}
+                />
+                {!(icons) || icons[i] && (
+                  <div
+                    className="absolute grid grid-cols-1 w-full h-full items-center justify-items-center invisible opacity-0 duration-200 ease-linear group-hover:visible group-hover:opacity-100">
+                    <div className="h-8 aspect-square">
+                      <Image width="100%" height="100%" src={`/tech-icons/${icons[i]}`} />
+                    </div>
+                  </div>
+                )}
+              </div>
             ))
           ) : (
             <motion.img
-              variants={{
-                hover: {
-                  scale: 1.05,
-                },
-              }}
-              className="rounded-md"
+              className="rounded-md duration-200 ease-linear group-hover:blur-sm"
               src={imgSrc}
               alt="img"
               draggable={false}
             />
           )}
-        </div>
+          {imgSrcs === undefined && (
+            <div
+              className="absolute grid grid-cols-6 w-full h-full items-center justify-items-center invisible opacity-0 duration-200 ease-linear group-hover:visible group-hover:opacity-100">
+              {icons?.map((icon, i) => (
+                <div
+                  className={`h-8 aspect-square 
+                  ${(icons?.length % 3 === 2 && (i === icons.length - 2 || i === icons.length - 1)) && "col-span-3"} 
+                  ${(icons?.length % 3 === 2 && (i !== icons.length - 2 || i !== icons.length - 1)) && "col-span-2"} 
+                  ${(icons?.length % 3 === 1 && (icons?.length !== 1)) && "col-span-3"} 
+                  ${(icons?.length === 1) && "col-span-6"} 
+                  ${icons?.length % 3 === 0 && "col-span-2"}`
+                  }>
+                  <Image width="100%" height="100%" src={`/tech-icons/${icon}`} />
+                </div>
+              ))}
+            </div>
+          )}
+        </motion.div>
       </div>
       <div className="mt-5 flex flex-col content-center items-center">
         <motion.h1
@@ -238,7 +266,7 @@ const WorkCard = ({
         </motion.p>
       </div>
     </motion.a>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
