b1b47f96a1c7dbdb10ab81e29c690b340ef8a0dc
KeiHara <84754523+KeiHara@users.noreply.github.com>
2023-08-22 23:17:50 +1200

    adding email link

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,16 @@ const Home: NextPage = () => {
         <div>
           <h1 className="text-4xl font-bold  dark:text-neutral-200 text-neutral-800">Keisuke Hara</h1>
           <p className="dark:text-neutral-200 text-neutral-800">
-            Software Engineer (Frontend Engineer / Musician)
+            Software Engineer (Frontend Engineer / Musician) <br />
+            <a
+              draggable={false}
+              className="inline-block origin-bottom-right indent-0 tracking-wide text-sky-500 duration-200 ease-linear after:float-left after:h-0.5 after:w-full after:origin-center after:scale-0 after:rounded-sm after:bg-sky-700 after:duration-200 hover:text-sky-600 hover:after:scale-100  hover:after:bg-sky-600 dark:text-teal-300 after:dark:bg-teal-300 hover:dark:text-teal-200 hover:after:dark:bg-teal-200"
+              href="mailto: keihara453@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              keihara453@gmail.com
+            </a>
           </p>
         </div>
         {/* prof pic */}
